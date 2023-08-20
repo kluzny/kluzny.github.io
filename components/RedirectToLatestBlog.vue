856ae9f1eb62d3f6d3561b6aff1e8d0d24164070
Kyle Luzny <kluzny@gmail.com>
2023-08-19 21:17:23 -0500

    try a convoluted redirect strategy to deal with prerenderer challenges

---
+++ components/RedirectToLatestBlog.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+onMounted(() => {
+  navigateTo('/posts/hello-world');
+});
+</script>
+
+<template>
+  <div></div>
+</template>
--- pages/index.vue
+++ pages/index.vue
@@ -2,5 +2,8 @@
 </script>
 
 <template>
-  <NuxtPage name='posts-hello-world' />
+  <h1 class="font-bold my-12">Welcome...</h1>
+  <ClientOnly>
+    <RedirectToLatestBlog />
+  </ClientOnly>
 </template>

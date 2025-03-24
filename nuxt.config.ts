955da0162cb04d5513fc16e464e1ede2ed577fed
Kyle Luzny <kluzny@gmail.com>
2025-03-24 06:24:07 -0500

    nuxt bootstrap

---
+++ nuxt.config.ts
@@ -0,0 +1,15 @@
+// https://nuxt.com/docs/api/configuration/nuxt-config
+export default defineNuxtConfig({
+  compatibilityDate: '2024-11-01',
+  devtools: { enabled: true },
+
+  modules: [
+    '@nuxt/eslint',
+    '@nuxt/icon',
+    '@nuxt/fonts',
+    '@nuxt/content',
+    '@nuxt/image',
+    '@nuxt/test-utils',
+    '@nuxt/ui'
+  ]
+})

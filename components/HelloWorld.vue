a33924f7e3e22bb31a36bc805043033f12f954e4
Kyle Luzny <kluzny@gmail.com>
2025-03-24 07:24:09 -0500

    install tailwind

---
+++ components/HelloWorld.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts">
+  const count = ref(1)
+  const double = computed(() => count.value * 2)
+</script>
+
+<template>
+  <h1 class="underline">Hello World {{double}}</h1>
+</template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,8 +1,9 @@
+import tailwindcss from '@tailwindcss/vite';
+
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
   devtools: { enabled: true },
-
   modules: [
     '@nuxt/eslint',
     '@nuxt/icon',
@@ -11,5 +12,13 @@ export default defineNuxtConfig({
     '@nuxt/image',
     '@nuxt/test-utils',
     '@nuxt/ui'
-  ]
+  ],
+  css: [
+    '~/assets/css/main.css',
+  ],
+  vite: {
+    plugins: [
+      tailwindcss(),
+    ]
+  }
 })

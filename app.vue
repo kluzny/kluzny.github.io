a4322fbf25e8d1f99cdca700a47d3efb23bf2a49
Kyle Luzny <kluzny@gmail.com>
2025-03-25 11:09:57 -0500

    dark theme

--- app.vue
+++ app.vue
@@ -1,13 +1,60 @@
+<script setup>
+useHead({
+  bodyAttrs: {
+    class: 'bg-white text-black dark:bg-black dark:text-lime-500'
+  }
+});
+
+const posts = [
+  { name: 'Hello World!', path: '/posts/hello-world' },
+];
+</script>
+
 <template>
-  <div>
-    <NuxtRouteAnnouncer />
+  <NuxtRouteAnnouncer />
+  <div class="flex justify-center items-start space-x-2 mt-8 bg-black">
+    <nav class="flex flex-col justify-start items-start border-r-4 border-lime-500 p-4">
+      <ul class="flex flex-col space-y-2 mb-2 w-full">
+        <li class="text-lg border-b-2 link">
+          <NuxtLink to="/" class="block w-full font-bold">
+            kyle@localhost $ <span class="text-2xl blink">▮</span>
+          </NuxtLink>
+        </li>
+        <li class="text-lg border-b-2 link">
+          <NuxtLink to="/about" class="block w-full">
+            About
+          </NuxtLink>
+        </li>
+      </ul>
+      <ul v-for="link in posts" class="flex flex-col space-y-2 w-full">
+        <li class="text-lg border-b-2 link">
+          <NuxtLink :to="link.path" class="block w-full">
+            {{ link.name }}
+          </NuxtLink>
+        </li>
+      </ul>
+    </nav>
+    <section class='rounded-sm p-4 w-100'>
+      <NuxtPage />
+    </section>
   </div>
-  <section>
-    <NuxtLink to="/">Home</NuxtLink> |
-    <NuxtLink to="/posts/hello-world">Hello World!</NuxtLink> |
-    <NuxtLink to="/about">About</NuxtLink>
-  </section>
-  <section>
-    <NuxtPage />
-  </section>
 </template>
+
+<style>
+  .blink {
+    animation: blink-animation 1s steps(5, start) infinite;
+    -webkit-animation: blink-animation 1s steps(5, start) infinite;
+  }
+
+  @keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+
+  @-webkit-keyframes blink-animation {
+    to {
+      visibility: hidden;
+    }
+  }
+</style>

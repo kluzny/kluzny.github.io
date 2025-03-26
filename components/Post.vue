f09ed6abe12a72d7fbcbb5d99eacbaaf02cbdaa2
Kyle Luzny <kluzny@gmail.com>
2025-03-26 15:28:13 -0500

    some default styles for markdown posts

--- components/Post.vue
+++ components/Post.vue
@@ -14,11 +14,80 @@ const post = await content(props.content);
 </script>
 
 <template>
-  <ContentRenderer v-if="post" :value="post" />
-  <div v-else class="flex items-center">
-    <NuxtImg src="/tux_head_sad.png"
-            class="inline"
-            alt="emoji sized pixelated bust of tux the linux penguin, portrayed sadly with a single tear." />
-    <h2>no really, post not found</h2>
-  </div>
+  <section class="post">
+    <ContentRenderer v-if="post" :value="post" />
+    <div v-else class="flex items-center">
+      <NuxtImg src="/tux_head_sad.png"
+              class="inline"
+              alt="emoji sized pixelated bust of tux the linux penguin, portrayed sadly with a single tear." />
+      <h2>no really, post not found</h2>
+    </div>
+  </section>
 </template>
+
+<style>
+@reference "../assets/css/main.css";
+
+.post {
+  h1 {
+    @apply text-4xl;
+  }
+
+  h2 {
+    @apply text-2xl;
+  }
+
+  h1, h2, h3 {
+    @apply font-bold;
+  }
+
+  h1, h2, h3,
+  p, pre, blockquote {
+    @apply mb-2;
+  }
+
+  h2 {
+    @apply mt-4;
+  }
+
+  a {
+    @apply underline underline-offset-2;
+  }
+
+  h3 a {
+    @apply no-underline;
+  }
+
+  ul, ol {
+    @apply list-inside pl-4;
+  }
+
+  ul {
+    @apply list-disc;
+  }
+
+  ol {
+    @apply list-decimal;
+  }
+
+  blockquote {
+    @apply py-4 px-8 border-l-4;
+    @apply bg-stone-100 dark:bg-stone-800;
+    @apply border-stone-500 dark:border-stone-500;
+  }
+
+  blockquote ul {
+    @apply list-none;
+  }
+
+  blockquote ul li {
+    @apply text-right font-bold italic;
+  }
+
+  pre {
+    @apply p-4;
+    @apply bg-stone-800 text-stone-100;
+    @apply border-2 border-stone-500 rounded-sm;
+  }
+}
+</style>
--- content/markdown.md
+++ content/markdown.md
@@ -4,9 +4,9 @@ title: 'Markdown Demo'
 description: 'A demo markdown post'
 ---
 
-# Markdown Demo
+# {{ $doc.title }}
 
-This post was written in markdown using [@nuxt/content](https://content.nuxt.com)
+This post was written in markdown using [@nuxt/content 🌐](https://content.nuxt.com)
 
 ## Use Cases
 
@@ -14,22 +14,39 @@ This post was written in markdown using [@nuxt/content](https://content.nuxt.com
 * markdown
 * styling
 
+## Ordered Lists
+
+1. foo
+2. bar
+3. baz
+    1. qux
+    1. quux
+    1. quuux
+
 ## Lorem Ipsum
 
 Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
 
-thanks [lorem ipsum generator](https://loremipsum.io)
+Thanks [lorem ipsum generator 🌐](https://loremipsum.io)
 
-# Code Blocks
+## Code Blocks
 
+### bash
 ```bash
 echo "hello linux community!"
 ```
 
+### ruby
 ```ruby
 puts "hello ruby community!"
 ```
 
+### code
 ```
 print("hello additional languages that print strings")
 ```
+
+## Quotes
+
+> If debugging is the process of removing bugs, then programming must be the process of putting them in.
+> - Edsger W. Dijkstra

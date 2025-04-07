9c8101b94ab08cca6bc28d37daaea21f11e5c8c0
Kyle Luzny <kluzny@gmail.com>
2025-04-07 16:37:40 -0500

    a simple tarpit

---
+++ pages/tarpit/[id].vue
@@ -0,0 +1,16 @@
+<script setup lang="ts">
+const route = useRoute();
+const { id } = route.params;
+
+const uuid = self.crypto.randomUUID();
+const path = `/tarpit/${uuid}`;
+</script>
+
+<template>
+<p>
+  {{id}}
+  <NuxtLink :to="path" class='link'>
+    {{uuid}}
+  </NuxtLink>
+</p>
+</template>

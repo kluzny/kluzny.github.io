9db572c4540579bd2a50d55d830214c029f28663
Kyle Luzny <kluzny@gmail.com>
2025-04-07 16:44:12 -0500

    deterministic link generation

--- pages/tarpit/[id].vue
+++ pages/tarpit/[id].vue
@@ -2,7 +2,11 @@
 const route = useRoute();
 const { id } = route.params;
 
-const uuid = self.crypto.randomUUID();
+
+import { v5 as uuidv5 } from 'uuid';
+const NAMESPACE = '716c2014-8839-49bc-bffe-f3c2ebc38824';
+
+const uuid = uuidv5(id, NAMESPACE);
 const path = `/tarpit/${uuid}`;
 </script>
 
